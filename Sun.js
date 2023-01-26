36a78986d599bba1a724e4494ae2cc8d92ace71a
atliylol <84818797+Atliylol@users.noreply.github.com>
2023-01-26 00:52:54 -0300

    Sun 0.0.1

--- Sun.js
+++ Sun.js
@@ -1,1 +1,20 @@
+import fire from energy;
+import year from calendar;
 
+let temperature = 5778;
+let mass = 1988920000000000000000000000000; // kg
+let size = 696340; // km
+let state = fire();
+
+if (year >= 600000002030) {
+  while (size < 500000000) {
+    redGiant();
+  }
+}
+
+
+function redGiant() {
+  size++;
+  mass++;
+  temperature++;
+}
